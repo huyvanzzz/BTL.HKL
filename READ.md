7a17ecf371f956144c9ba496ba6542c5a957e1ac
Nguyen Van Huy <23020379@vnu.edu.vn>
2024-03-29 18:35:14 +0700

    first commit

---
+++ READ.md
@@ -0,0 +1,1 @@
+"our BTL with joining of Nguyen Van Huy,Tran Quoc Khanh,Nguyen Van Linh." 
---
+++ src/Word.java
@@ -0,0 +1,25 @@
+public class AddWord {
+    private String key;
+    private String def;
+
+    public AddWord(String key, String def) {
+        this.key = key;
+        this.def = def;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getDef() {
+        return def;
+    }
+
+    public void setDef(String def) {
+        this.def = def;
+    }
+}
